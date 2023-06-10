cb0d03da3fb1fbbf7e5f87f4746df425a3cc69ef
NecipAkgz <nexco@hotmail.com>
2023-06-11 01:21:47 +0300

    feat(frontend): use react-query to fetch data in EditNote, NewNote, and EditUser components The EditNote, NewNote, and EditUser components now use the useGetNotesQuery and useGetUsersQuery hooks from the notesApiSlice and usersApiSlice respectively to fetch data using react-query. This improves the performance of the components by reducing the number of unnecessary re-renders. The components also now display a loading spinner while the data is being fetched. Additionally, the EditNote component now checks if the user has access to edit the note before rendering the EditNoteForm.

--- frontend/src/features/notes/EditNote.js
+++ frontend/src/features/notes/EditNote.js
@@ -1,17 +1,40 @@
 import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { selectNoteById } from './notesApiSlice'
-import { selectAllUsers } from '../users/usersApiSlice'
+import PulseLoader from 'react-spinners/PulseLoader'
+import useAuth from '../../hooks/useAuth'
+import useTitle from '../../hooks/useTitle'
+import { useGetUsersQuery } from '../users/usersApiSlice'
 import EditNoteForm from './EditNoteForm'
+import { useGetNotesQuery } from './notesApiSlice'
 
 const EditNote = () => {
-    const { id } = useParams()
+  useTitle('techNotes: Edit Note')
 
-    const note = useSelector(state => selectNoteById(state, id))
-    const users = useSelector(selectAllUsers)
+  const { id } = useParams()
 
-    const content = note && users ? <EditNoteForm note={note} users={users} /> : <p>Loading...</p>
+  const { username, isManager, isAdmin } = useAuth()
 
-    return content
+  const { note } = useGetNotesQuery('notesList', {
+    selectFromResult: ({ data }) => ({
+      note: data?.entities[id],
+    }),
+  })
+
+  const { users } = useGetUsersQuery('usersList', {
+    selectFromResult: ({ data }) => ({
+      users: data?.ids.map((id) => data?.entities[id]),
+    }),
+  })
+
+  if (!note || !users?.length) return <PulseLoader color={'#FFF'} />
+
+  if (!isManager && !isAdmin) {
+    if (note.username !== username) {
+      return <p className='errmsg'>No access</p>
+    }
+  }
+
+  const content = <EditNoteForm note={note} users={users} />
+
+  return content
 }
 export default EditNote
