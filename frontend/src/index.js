ad5fdf50ebd92d3bab7b6cf49c32c445801fbb25
NecipAkgz <nexco@hotmail.com>
2023-06-11 01:25:17 +0300

    build(frontend): update dependencies and disable React dev tools in production The `@fvilers/disable-react-devtools` package was added to disable React dev tools in production. The `baseUrl` in `apiSlice.js` was changed to the production API URL. The `devTools` option in `store.js` was set to `false` to disable the Redux dev tools in production.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,4 @@
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
@@ -6,6 +7,8 @@ import './index.css'
 
 import { Provider } from 'react-redux'
 import { store } from './app/store'
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
